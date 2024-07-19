c7c4050e85f004f56983f41b84485a5e58f02fcd
rick <r_c2@qq.com>
2024-07-19 23:47:12 +0800

    test: 尝试删除初始化pages config

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+// 允许 nextJs 识别 mdx 并作为组件处理
+import nextMDX from '@next/mdx';
+
+const withMdx = nextMdx({
+  options: {
+    /* otherOptions… */
+  },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withMdx({
+  // Support MDX files as pages:
+  pageExtensions: ['md', 'mdx', 'tsx', 'ts', 'jsx', 'js'],
+  output: 'export',
+});
+
+export default nextConfig;
