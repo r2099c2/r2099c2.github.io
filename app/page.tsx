8937dece591cbddb380fde431f0ebfdecca7c20c
rick <r_c2@qq.com>
2024-07-24 23:50:37 +0800

    feat: Update metadata titles and descriptions

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
 import { getPost } from '@/lib/helper';
 import NavList from './components/NavList';
+import Head from 'next/head';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '播客首页',
+};
 
 export default async function Home() {
   const { posts, postsByMonth } = await getPost();
 
-  console.log(posts);
-
   return (
-    <main className="flex flex-row w-full pt-12">
-      <div className="hidden md:block md:w-1/5 pl-6"></div>
-      <div className="w-full md:w-3/5 px-6">
-        <NavList posts={posts} />
-      </div>
-    </main>
+    <>
+      <main className="flex flex-row w-full pt-12">
+        <div className="hidden md:block md:w-1/5 pl-6"></div>
+        <div className="w-full md:w-3/5 px-6">
+          <NavList posts={posts} />
+        </div>
+      </main>
+    </>
   );
 }
