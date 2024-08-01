140b8668c468dc5f4f02d8bd29d05be6ca0f5e40
rick <r_c2@qq.com>
2024-08-01 21:43:17 +0800

    feat: use https://github.com/timlrx/tailwind-nextjs-starter-blog

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,9 @@
-import { getPosts } from '@/lib/helper';
-import NavList from '@/components/NavList';
-import TimeLine from '@/components/TimeLine';
-import { Metadata } from 'next';
+import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
+import { allBlogs } from 'contentlayer/generated'
+import Main from './Main'
 
-export const metadata: Metadata = {
-  title: '播客首页',
-};
-
-export default async function Home() {
-  const { posts, postsByMonth } = await getPosts();
-
-  return (
-    <>
-      <main className="flex flex-row w-full pt-12">
-        <div className="hidden md:block md:w-1/5 pl-6"></div>
-        <div className="w-full md:w-3/5 px-6">
-          <NavList posts={posts} />
-        </div>
-        <div className="hidden md:flex justify-end md:w-1/5 pr-6 text-right">
-          <TimeLine postsByMonth={postsByMonth} />
-        </div>
-      </main>
-    </>
-  );
+export default async function Page() {
+  const sortedPosts = sortPosts(allBlogs)
+  const posts = allCoreContent(sortedPosts)
+  return <Main posts={posts} />
 }
