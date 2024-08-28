966a4cc8fe2c23518a4d4b71e4e82964a43fdd08
rick <r_c2@qq.com>
2024-08-28 23:45:42 +0800

    feat: 增加 mdx 相关能力和库

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+// next.config.js 配置：
+// 允许 nextJs 识别 mdx 并作为组件处理
+import nextMdx from '@next/mdx';
+
+const withMdx = nextMdx({
+  // By default only the `.mdx` extension is supported.
+  extension: /\.mdx?$/,
+  options: {
+    /* otherOptions… */
+  },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withMdx({
+  // Support MDX files as pages:
+  pageExtensions: ['md', 'mdx', 'tsx', 'ts', 'jsx', 'js'],
+});
 
 export default nextConfig;
