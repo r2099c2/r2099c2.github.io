00f5fd45b6f2969a82adbd8535189dd86ab30c56
rick <r_c2@qq.com>
2024-09-03 23:45:51 +0800

    feat: 首页展示 post title

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,18 @@
 import { getPosts } from '@/utils/tools';
 
 export default async function Home() {
-  const posts = await getPosts();
+  const { posts } = await getPosts();
   console.log('🚀 ~ Home ~ posts:', posts);
 
   return (
     <main>
       {/* 列表 */}
       <ul>
-        <li>
-          <a href="/posts/hello-world">Hello, World!</a>
-        </li>
-        <li>
-          <a href="/posts/another-post">Another Post</a>
-        </li>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <a href={`/posts/${post.slug}`}>{post.title}</a>
+          </li>
+        ))}
       </ul>
     </main>
   );
