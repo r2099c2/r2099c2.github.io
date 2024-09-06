c6d0c34a82f74d163203dc217880f1f94ea0a660
rick <r_c2@qq.com>
2024-09-06 22:10:08 +0800

    style: 增加一点样式，太难设计了

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,17 @@ export default async function Home() {
   console.log('🚀 ~ Home ~ posts:', posts);
 
   return (
-    <main className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
+    <main className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0 py-10">
       {/* 列表 */}
       <ul>
         {posts.map((post) => (
           <li key={post.slug}>
-            <a href={`/post/${post.slug}`}>{post.title}</a>
+            <a
+              className="hover:bg-slate-400/30 p-5 rounded-xl"
+              href={`/post/${post.slug}`}
+            >
+              {post.title}
+            </a>
           </li>
         ))}
       </ul>
