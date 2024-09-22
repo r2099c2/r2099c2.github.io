e3ce820e2da9c1fb09e92c5d4af44a66dbaebd88
rick <r_c2@qq.com>
2024-09-22 23:05:16 +0800

    style: 文章的导航增加一些样式

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -23,22 +23,24 @@ const PostDetail = async ({ params }: { params: { slug: string } }) => {
   return (
     <div className="max-w-8xl min-h-screen max-auto pt-12 px-4 sm:px-6 md:px-8 bg-sky-900">
       {/* 左侧文章切换 */}
-      <div className="hidden lg:block w-[14rem] fixed px-4">
+      <ul className="hidden lg:block w-[14rem] fixed space-y-6 lg:space-y-2 border-l border-slate-100">
         {posts.map((post) => (
-          <a
-            key={post.slug}
-            className={clsx('text-sm block pb-1', {
-              'text-blue-400': post.slug === slug,
-            })}
-            href={`/post/${post.slug}`}
-          >
-            {post.title} - {post.month}
-          </a>
+          <li key={post.slug}>
+            <a
+              className={clsx('text-sm block pl-3 -ml-px', {
+                'text-blue-400 border-l border-current font-semibold':
+                  post.slug === slug,
+              })}
+              href={`/post/${post.slug}`}
+            >
+              {post.title} - {post.month}
+            </a>
+          </li>
         ))}
-      </div>
+      </ul>
       {/* 中间当前文章 */}
       <div className="lg:pl-[14rem] overflow-hidden">
-        <article id="article">
+        <article id="article" className="px-4">
           <h1>{post.data.title}</h1>
           <MDXRemote source={post.content} components={MDXComponents} />
         </article>
