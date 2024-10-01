bccd22f4f77f47b296f9d90112267c71c593cd06
rick <r_c2@qq.com>
2024-10-01 21:09:56 +0800

    style: 整个简单的首页样式

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,22 @@ export default async function Home() {
   const { posts } = await getPosts();
 
   return (
-    <main className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0 py-10">
-      {/* 列表 */}
-      <ul>
-        {posts.map((post) => (
-          <li key={post.slug}>
-            <a
-              className="hover:bg-slate-400/30 p-5 rounded-xl"
-              href={`/post/${post.slug}`}
-            >
-              {post.title}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </main>
+    <div className="w-screen h-screen bg-sky-900">
+      <main className="mx-auto pt-10 px-5">
+        {/* 列表 */}
+        <ul>
+          {posts.map((post) => (
+            <li key={post.slug}>
+              <a
+                className="hover:bg-slate-400/30 p-5 rounded-xl"
+                href={`/post/${post.slug}`}
+              >
+                {post.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
   );
 }
